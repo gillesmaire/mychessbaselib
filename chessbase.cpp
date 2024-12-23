67f2bf31db30f4e545c9b8f9c2246322111a81c2
Gilles Maire <gilles@gillesmaire.com>
2024-12-23 14:54:01 +0100

    sc_base_gameslist ported to gamelist in Qt Style

--- chessbase.cpp
+++ chessbase.cpp
@@ -2,16 +2,21 @@
 #include "dbasepool.h"
 #include "scidbase.h"
 #include "progressbar.h"
-#include "string_view"
 #include "misc.h"
 
-errorT ChessBase::open(QString codec, fileModeT fmode, QString filename, int &numberbase)
+#include <QList>
+#include <QPair>
+
+errorT ChessBase::open(QString filename,ICodecDatabase::Codec codec, fileModeT fmode,  int &numberbase)
 {
     if (DBasePool::find(filename.toStdString().c_str()) ) return (ERROR_FileInUse) ;
     auto dbase=DBasePool::getFreeSlot();
     if (!dbase ) return (ERROR_Full);
-    errorT err=dbase->open(codec.toStdString().c_str(),fmode,filename.toStdString().c_str());
-    mProgressBar=  new ProgressBar();
+    ProgressBar pb;
+    errorT err=dbase->open(CodecEnum2Str(codec),fmode,filename.toStdString().c_str(),pb);
+    if ((err == ERROR_FileOpen || err == ERROR_FileMode) &&
+	    fmode == FMODE_Both)
+	    err = dbase->open(CodecEnum2Str(codec), FMODE_ReadOnly, filename.toStdString().c_str(), pb);
     if ( err != OK && err != ERROR_NameDataLoss ) return err;
     numberbase = DBasePool::switchCurrent(dbase);
     return err;
@@ -21,15 +26,13 @@ errorT  ChessBase::close( scidBaseT *dbase)
 {
     if (dbase->getFileName() == "<clipbase>") return  ERROR_BadArg;
 	dbase->Close();
-	delete (mProgressBar);
 	return OK;
 }
 
 errorT ChessBase::compact(scidBaseT *dbase)
 {
-
-    mProgressBar = new ProgressBar();
-    int err=dbase->compact(*mProgressBar);
+    ProgressBar pb;
+    int err=dbase->compact(pb);
     return  err;
 }
 
@@ -53,8 +56,8 @@ errorT ChessBase::copyGames(scidBaseT *sourcebase, QString NumGameOrFilterName, 
     
     const HFilter filter = sourcebase->getFilter(NumGameOrFilterName.toStdString());
     if ( filter != 0 ){
-         mProgressBar=  new ProgressBar();
-         err=targetBase->importGames(sourcebase, filter, *mProgressBar);
+         ProgressBar pb;
+         err=targetBase->importGames(sourcebase, filter,pb);
         }
     else {
          bool ok;
@@ -67,3 +70,266 @@ errorT ChessBase::copyGames(scidBaseT *sourcebase, QString NumGameOrFilterName, 
     }
     return err;
 }
+
+errorT ChessBase::create(QString name ,ICodecDatabase::Codec codec, int &numberbase)
+{
+    return open(name,codec,FMODE_Create,numberbase);
+}
+
+QList<QPair<QString, QString> > ChessBase::getExtraTags(scidBaseT *dbase)
+{   
+    auto list = dbase->getExtraInfo();
+    QList <QPair<QString, QString>> result;
+    for (const auto& e : list)
+    {
+        QPair<QString, QString> p(QString(e.first),QString(e.second.c_str()));
+        result<<p;
+    }
+    return result;
+}
+
+errorT ChessBase::setExtraTags(scidBaseT *dbase,QString tag, QString value)
+{
+   return dbase->setExtraInfo(tag.toStdString().c_str(),value.toStdString().c_str()) ;
+}
+
+QString ChessBase::filename(scidBaseT *dbase)
+{
+    return QString(dbase->getFileName().c_str());
+}
+
+
+std::string ChessBase::CodecEnum2Str(ICodecDatabase::Codec codec)
+{
+    if ( codec==ICodecDatabase::MEMORY ) return ("Memory");
+    else if (codec==ICodecDatabase::PGN ) return ("PNG");
+    else if (codec==ICodecDatabase::SCID4 ) return ("SCID4");
+    else  return ("SCID5");
+}
+
+QString ChessBase::CodecEnum2QString(ICodecDatabase::Codec codec)
+{   
+    std::string st=CodecEnum2Str(codec);
+    return (QString (st.c_str()));
+}
+
+errorT ChessBase::getGameFlag(scidBaseT *dbase, uint gamenum, char flagtype , bool &result)
+{
+
+   uint flag = IndexEntry::CharToFlagMask(flagtype);
+   if ( gamenum > 0 && gamenum < dbase->numGames())
+        result = dbase->getFlag(flag,gamenum-1);
+   else return  ERROR_NumGamesLimit ;
+   return OK;
+  
+}
+
+errorT ChessBase::setGameFlag(scidBaseT *dbase, uint gamenum, char flagtype)
+{
+    uint flag = IndexEntry::CharToFlagMask(flagtype);
+    if ( gamenum > 0 && gamenum < dbase->numGames())
+       dbase->setFlag(true,flag,gamenum-1);
+    else return  ERROR_NumGamesLimit ;
+    return OK;
+}
+
+errorT ChessBase::unsetGameFlag(scidBaseT *dbase, uint gamenum, char flagtype)
+{
+    uint flag = IndexEntry::CharToFlagMask(flagtype);
+    if ( gamenum > 0 && gamenum < dbase->numGames())
+       dbase->setFlag(false,flag,gamenum-1);
+    else return  ERROR_NumGamesLimit ;
+    return OK;
+}
+
+errorT ChessBase::InvertGameFlag(scidBaseT *dbase, uint gamenum, char flagtype)
+{
+    uint flag = IndexEntry::CharToFlagMask(flagtype);
+    if ( gamenum > 0 && gamenum < dbase->numGames())
+       dbase->invertFlag(flag,gamenum-1);
+    else return  ERROR_NumGamesLimit ;
+    return OK;
+}
+
+errorT ChessBase::gameCommandFlag(scidBaseT *dbase, QString filt, CommandFlag command, char flagtype)
+{
+    uint flagType = IndexEntry::CharToFlagMask(flagtype);
+	if (flagType != 0 && command != CommandFlag::nothing )  {
+		const HFilter filter = dbase->getFilter(filt.toStdString().c_str());
+		if (filter != 0) {
+			switch (command) {
+			case set:  dbase->setFlags(true, flagType, filter); return OK;
+			case unset:  dbase->setFlags(false, flagType, filter);return OK;
+			case invert: dbase->invertFlags(flagType, filter);return OK;
+			case nothing: ;
+			case get : ;
+			}
+		} else {
+			gamenumT gNum = strGetUnsigned(filt.toStdString().c_str());
+			if (gNum > 0 && gNum <= dbase->numGames()) {
+				gNum--;
+				switch (command) {
+				case 1: dbase->getFlag(flagType, gNum); return OK;
+				case 2: dbase->setFlag(true, flagType, gNum) ; return OK;
+				case 3: dbase->setFlag(false, flagType, gNum);return OK;
+				case 4: dbase->invertFlag(flagType, gNum); return OK;
+				case nothing : return ERROR_NumGamesLimit;
+				}
+			}
+		}
+	}  
+	return ERROR_NumGamesLimit;
+}
+
+
+errorT ChessBase::gamelocation(scidBaseT *dbase, QString filt, QString sortCrit, uint gnumber , uint &result)
+{
+    const HFilter filter = dbase->getFilter(filt.toStdString().c_str());
+    if (filter == 0) return ERROR_BadArg;
+    result = dbase->sortedPosition(sortCrit.toStdString().c_str(), filter, gnumber - 1);
+	if (result == INVALID_GAMEID)
+		return ERROR_InvalidFEN ;
+	return  OK; 
+   
+}
+
+
+///
+/// \brief gamelocation find the first game that contains text in white name or black name of  event name or site name 
+/// \param dbase : pointer on base
+/// \param filt :  a string egal gameNum or filtername or all
+/// \param sort : the order for the list of games to search
+/// \param text : text of white name nlack name or whit or black event or site
+/// \param start  :  offset to begining to search 
+/// \param forwardir : if true search from startpos tp num game else search from 0 to num game
+/// \param result the position ( 0 for first ) if return is OK  
+/// \return 0 for not error else a value given by error.h
+///
+errorT ChessBase::gamelocation (scidBaseT *dbase, QString filt, QString sort, QString text, uint start, bool forwardir , uint &result)
+{
+   const HFilter filter = dbase->getFilter(filt.toStdString().c_str());
+   if (filter == 0) return ERROR_BadArg;
+   size_t location = INVALID_GAMEID;
+   auto contains = [&](gamenumT g) {
+			const auto tags = dbase->tagRoster(g);
+			return strAlphaContains(tags.white, text.toStdString().c_str()) ||
+			       strAlphaContains(tags.black, text.toStdString().c_str()) ||
+			       strAlphaContains(tags.event, text.toStdString().c_str()) ||
+			       strAlphaContains(tags.site, text.toStdString().c_str());
+		};
+    if (forwardir)  {
+            std::vector<gamenumT> buf(
+			    start >= filter->size() ? 1 : filter->size() - start);
+			buf.resize(
+			    dbase->listGames(sort.toStdString().c_str(), start, buf.size(), filter, buf.data()));
+			auto it = std::find_if(buf.begin(), buf.end(), contains);
+			if (it != buf.end())
+				location = start + std::distance(buf.begin(), it);
+    }
+    else {
+    std::vector<gamenumT> buf(start ? start : 1);
+			buf.resize(dbase->listGames(sort.toStdString().c_str(), 0, start, filter, buf.data()));
+			auto it = std::find_if(buf.rbegin(), buf.rend(), contains);
+			if (it != buf.rend())
+				location = std::distance(it, buf.rend()) - 1;
+    }
+    if (location == INVALID_GAMEID)
+		return ERROR_InvalidFEN ;
+    result=location;
+	return  OK; 
+}
+
+
+errorT ChessBase::gameslist(scidBaseT* dbase, uint start, uint  count, QString filt, QString sort , QList<QVariant>  &res)
+{
+   
+
+	const HFilter filter = dbase->getFilter(filt.toStdString().c_str());
+	if (filter == NULL)
+		return  ERROR_BadArg;
+	gamenumT* idxList = new gamenumT[count];
+	count = dbase->listGames(sort.toStdString().c_str(), start, count, filter, idxList);
+
+//	QList<QVariant> res (count * 3);
+	QList<QVariant> ginfo(24);
+	for (uint i = 0; i < count; ++i) {
+		uint idx = idxList[i];
+
+		ASSERT(filter->get(idx) != 0);
+		uint ply = filter->get(idx) -1;
+
+		const IndexEntry* ie = dbase->getIndexEntry(idx);
+		const auto tags = dbase->tagRoster(*ie);
+
+		ginfo.clear();
+		ginfo.push_back(idx +1);
+		ginfo.push_back(RESULT_STR[ie->GetResult()]);
+		ginfo.push_back((ie->GetNumHalfMoves() + 1) / 2);
+		ginfo.push_back(tags.white);
+		std::string eloStr;
+		eloT welo = ie->GetWhiteElo();
+		if (welo != 0) {
+			eloStr = std::to_string(welo);
+		} else {
+			welo = dbase->peakElo(ie->GetWhite());
+			eloStr = std::to_string(welo);
+			if (welo != 0) {
+				eloStr.insert(eloStr.begin(), '(');
+				eloStr.insert(eloStr.end(), ')');
+			}
+		}
+		
+		ginfo.push_back(QString::fromUtf8(eloStr));
+		ginfo.push_back(tags.black);
+		eloT belo = ie->GetBlackElo();
+		if (belo != 0) {
+			eloStr = std::to_string(belo);
+		} else {
+			belo = dbase->peakElo(ie->GetBlack());
+			eloStr = std::to_string(belo);
+			if (belo != 0) {
+				eloStr.insert(eloStr.begin(), '(');
+				eloStr.insert(eloStr.end(), ')');
+			}
+		}
+		ginfo.push_back(QString::fromUtf8(eloStr));
+		char buf_date[16];
+		date_DecodeToString(ie->GetDate(), buf_date);
+		ginfo.push_back(QString(buf_date));
+		ginfo.push_back(tags.event);
+		ginfo.push_back(tags.round);
+		ginfo.push_back(tags.site);
+		ginfo.push_back(ie->GetNagCount());
+		ginfo.push_back(ie->GetCommentCount());
+		ginfo.push_back(ie->GetVariationCount());
+		char deleted[2] = {0};
+		deleted[0] = (ie->GetDeleteFlag()) ? 'D' : ' ';
+		ginfo.push_back(QString(deleted));
+		char flags[16];
+		ie->GetFlagStr (flags, "WBMENPTKQ!?U123456");
+		ginfo.push_back(QString(flags));
+		ecoStringT ecoStr;
+		eco_ToExtendedString (ie->GetEcoCode(), ecoStr);
+		ginfo.push_back(QString(ecoStr));
+		std::string endMaterial = matsig_makeString(ie->GetFinalMatSig());
+		ginfo.push_back(QString::fromStdString(endMaterial));
+		char startpos[2] = {0};
+		startpos[0] = (ie->GetStartFlag()) ? 'S' : ' ';
+		ginfo.push_back(QString(startpos));
+		char buf_eventdate[16];
+		date_DecodeToString (ie->GetEventDate(), buf_eventdate);
+		ginfo.push_back(QString(buf_eventdate));
+		ginfo.push_back(ie->GetYear());
+		ginfo.push_back((welo + belo)/2);
+		ginfo.push_back(ie->GetRating());
+		ginfo.push_back(QString::fromUtf8(dbase->getGame(ie).getMoveSAN(ply, 10)));
+
+		res.push_back(QString::fromUtf8(std::to_string(idx+1) + "_" + std::to_string(ply)));
+		res.push_back(ginfo);
+		res.push_back(QString(deleted));
+	}
+
+	delete [] idxList;
+	return  OK;
+}
+
--- chessbase.h
+++ chessbase.h
@@ -5,6 +5,8 @@
 #include "error.h"
 #include "scidbase.h"
 #include "progressbar.h"
+#include <QList>
+#include <QPair>
 
 /// This Class is the Qt database management for Scid5 base with Qt and not with Tcl/Tk.
 /// the sc_base.cpp has be converted with 
@@ -12,20 +14,22 @@
 class ChessBase : public QObject
 {
 public:
+    enum CommandFlag { nothing, get, set, unset, invert };
+    
     ChessBase( QObject *ob);
     ///
     /// \brief open a base with value of codec  "MEMORY", "PGN", "SCID4","SCID5"
+    /// \param filename : name of filename 
     /// \param codec : can be "MEMORY", "PGN", "SCID4","SCID5"
     /// \param fmode : can be member of enum FMODE_None = 0,FMODE_ReadOnly,FMODE_WriteOnly,FMODE_Both,FMODE_Create
-    /// \param filename : name of filename 
     /// \param numberbase : a return parameter indicate the number of base selected
     /// \return :  0 if no error else a value given in error.h
-    errorT open( QString codec, fileModeT fmode ,QString filename, int  &numberbase);
+    errorT open(QString filename, ICodecDatabase::Codec codec, fileModeT fmode , int  &numberbase);
 
     /// 
-    /// \brief close the database given nhy the pointer
+    /// \brief close the database given by the pointer
     /// \param pointer on Base
-    /// \return  0 of not error else a value give by error.h 
+    /// \return  0 of not error else a value given by error.h 
     errorT close(scidBaseT *dbase);
 
     /// 
@@ -35,7 +39,7 @@ public:
     ///     - improve search operations 
     ///     - replace bad names by "???"
     /// \param pointer on base
-    /// \return 0 of not error else a value give by error.h 
+    /// \return 0 of not error else a value given by error.h 
     ///
     errorT compact( scidBaseT *dbase) ;
 
@@ -47,7 +51,7 @@ public:
     ///                     - unused games
     ///                     - parse optimizations
     ///                     - bad named 
-    /// \return 0 of not error else a value give by error.h 
+    /// \return 0 of not error else a value given by error.h 
     ///
     errorT statsInfoToBeCompacted(scidBaseT *dbase, QList<quint64> &list);
     
@@ -56,13 +60,122 @@ public:
     /// \param sourcebase  is a pointer of source dataBase
     /// \param NumGameOrFilterName : can be a number of game or a FilterName 
     /// \param targedBaseId : Id of the target base 
-    /// \return  0 of not error else a value give by error.h 
+    /// \return  0 of not error else a value given by error.h 
     ///
     errorT copyGames (scidBaseT *sourcebase, QString NumGameOrFilterName, uint  targedBaseId );
     
+    ///
+    /// \brief sc_base_create 
+    /// \param name for the base
+    /// \param codec MEMORY PGN SCID4 SCID5 
+    /// \return 0 of not error else a value given by error.h
+    ///
+    errorT create(QString name,ICodecDatabase::Codec codec, int &numberBase  );
     
-private:
-   ProgressBar * mProgressBar;
+    ///
+    /// \brief getExtraTags get le list of Pair of extratags 
+    /// \param dbase pointer on the scidBaseT 
+    /// \return  a QList of QPair<QString,QString>
+    ///
+    QList <QPair <QString,QString>>  getExtraTags(scidBaseT *dbase);
+    
+    ///
+    /// \brief setExtraTags set in the Database extra tag value
+    /// \param dbase : pointer on the base
+    /// \param tag : tag name  to set
+    /// \param value : tag value to set
+    /// \return 
+    ///
+    errorT setExtraTags(scidBaseT *dbase , QString  tag, QString value);
+    
+    ///
+    /// \brief filename returne the filename of database. 
+    ///     - for PGN retule the full path filename 
+    ///     - for scid base returne the name of base without the extention si4 or si5
+    ///     - for clipbase return <clipbase>
+    /// \param dbase pointer on the base 
+    /// \return 
+    ///
+    QString filename( scidBaseT *dbase);
+    ///
+    /// \brief CodecEnum2Str convert Enum PGN SCID5 SCID4 MEM to QString equivalent
+    /// \param codec the enum codec
+    /// \param numberBase : the number of base
+    /// \return the string in std::string format
+    ///
+    static std::string CodecEnum2Str( ICodecDatabase::Codec codec ) ;
+    
+    ///
+    /// \brief CodecEnum2Str convert Enum PGN SCID5 SCID4 MEM to QString equivalent
+    /// \param codec
+    /// \return the string in QString format
+    ///
+     QString CodecEnum2QString( ICodecDatabase::Codec codec ) ;
+    
+    ///
+    /// \brief getGameFlag get Flag flagtype of gamenum. For example D 
+    /// \param dbase* : pointer on the base
+    /// \param gamenum : game number
+    /// \param flagtype : 'D' for Delete 'S' start see IndexEntry::CharToFlagMask
+    /// \param result contain true if flag is true  or false else
+    /// \return 0 of not error else a value given by error.h
+    ///
+    errorT getGameFlag( scidBaseT * dbase, uint gamenum, char flagtype, bool &result );
+     
+    ///
+    /// \brief setGameFlag set a Flag for the game
+    /// \param dbase * : pointer on base
+    /// \param gamenum :  num of game to set he flag
+    /// \param flagtype : the type fo flag to set 
+    /// \return 0 of not error else a value given by error.h
+    ///
+    errorT setGameFlag( scidBaseT * dbase, uint gamenum, char flagtype );
+    
+    ///
+    /// \brief unsetGameFlag unset a Flag for the game
+    /// \param dbase * pointeur on base
+    /// \param gamenum  :  num of game to set he flag
+    /// \param flagtype : the type fo flag to set 
+    /// \return 0 of not error else a value given by error.h
+    ///
+    errorT unsetGameFlag(scidBaseT *dbase, uint gamenum, char flagtype);
+    
+    ///
+    /// \brief InvertGameFlag invert  a Flag for the game
+    /// \param dbase * pointeur on base
+    /// \param gamenum  :  num of game to invert he flag
+    /// \param flagtype : the type fo flag to invert
+    /// \return 0 of not error else a value given by error.h
+    ///
+    errorT InvertGameFlag(scidBaseT *dbase, uint gamenum, char flagtype);
+    
+    ///
+    /// \brief gameCommandFlag : allows same operation like setGameFlag, getGameFlag, unsetGameFlag
+    ///                          and invertGameFlag but allow to apply on gamenum but also on filtername
+    ///                          or all 
+    /// \param dbase * : pointer on base 
+    /// \param filter : a string egal gameNum or filtername or all 
+    /// \param command
+    /// \param flagtype
+    /// \return 
+    ///
+    errorT gameCommandFlag(scidBaseT *dbase, QString filter, CommandFlag command, char flagtype);
+    
+    
+    ///
+    /// \brief gamelocation find the position of a game by number of game
+    /// \param dbase : pointer on scidBaseT 
+    /// \param filt : a string egal gameNum or filtername or all
+    /// \param sortCrit : the order for the list of games to search
+    /// \param gnumber  : the number of game to search
+    /// \param result : the position ( 0 for first ) if return is OK
+    /// \return 0 for not error else a value given by error.h
+    ///
+    errorT gamelocation(scidBaseT *dbase, QString filt, QString sortCrit, uint gnumber, uint &result);
+    
+    errorT gamelocation(scidBaseT *dbase, QString filt, QString sort, QString text, uint start, bool forwardir, uint &result);
+    
+    errorT gameslist(scidBaseT *dbase, uint start, uint count, QString filt, QString sort, QList<QVariant> &res);
 };
 
 #endif // CHESSBASE_H
